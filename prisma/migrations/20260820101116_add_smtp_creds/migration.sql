bcae31efc9089a4688acca73dec6764468662302
Krisha Shah <krishashah3011@gmail.com>
2026-08-20 15:42:11 +0530

    added smtp migrations

---
+++ prisma/migrations/20260820101116_add_smtp_creds/migration.sql
@@ -0,0 +1,6 @@
+-- AlterTable
+ALTER TABLE "ShopSettings" ADD COLUMN "smtpFromEmail" TEXT;
+ALTER TABLE "ShopSettings" ADD COLUMN "smtpHost" TEXT;
+ALTER TABLE "ShopSettings" ADD COLUMN "smtpPass" TEXT;
+ALTER TABLE "ShopSettings" ADD COLUMN "smtpPort" TEXT;
+ALTER TABLE "ShopSettings" ADD COLUMN "smtpUser" TEXT;
